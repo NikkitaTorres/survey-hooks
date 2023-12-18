f889d400250f781e03b120e926f6015deb7d3e68
NikkitaTorres <nikkitatorres@yahoo.com>
2023-12-18 13:48:47 -0800

    update .gitignore to include .env

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import SurveyControl from './components/SurveyControl';
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <SurveyControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
